0cd479714a57762552baf31bc2fd4a23055c1558
zaqaq <617989597@qq.com>
2021-11-05 13:14:02 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<n-loading-bar-provider>
+  <n-message-provider>
+    <n-notification-provider>
+      <n-dialog-provider>
+        <demo />
+      </n-dialog-provider>
+    </n-notification-provider>
+  </n-message-provider>
+</n-loading-bar-provider>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import Demo from "./Demo.vue";
+
+export default defineComponent({
+components: {
+  Demo,
+},
+});
+</script>
